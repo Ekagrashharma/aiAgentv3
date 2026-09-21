43fcf4a36fb064925139bf692bd669f9db7dcd00
ekagrashharma <ekagrashharma@gmail.com>
2026-09-21 13:45:47 +0000

    TODO

--- src/tools/weatherTool.ts
+++ src/tools/weatherTool.ts
@@ -8,6 +8,7 @@ export const weatherTool = tool({
     description: "Get the current weather for a city. Use this when the user asks about the current weather or temperature of a city.",
     inputSchema: z.object({}),
     execute: async () => {
+        // make a function that take a data from api and give the response
             const response = await fetch(
                 "https://api.open-meteo.com/v1/forecast?latitude=28.36&longitude=79.43&current=temperature_2m,weather_code,wind_speed_10m"
                     );
